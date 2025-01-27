ca7e732c7ef2d2cd7a424517fd9a5de80ef04ac8
Neerad Uppalapati <neeru@umich.edu>
2025-01-27 00:07:05 +0000

    revert

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var name = "Neeru Uppalapati";
 var i = 0;
-var speed = 50; // typing speed in milliseconds
+var speed = 50; //typing speed in milliseconds
 
 function typeWriter() {
   if (i < name.length) {
     document.getElementById("name").innerHTML += name.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  } else {
+    // setTimeout(typeWriter2, 0); //wait for 1 second before starting second typing animation
   }
 }
 
 function typeWriter2() {
   var text = "Engineering @ IMC Trading";
+  // 〽️
   var j = 0;
-  var speed2 = 35; // typing speed in milliseconds for the second animation
+  var speed2 = 35; //typing speed in milliseconds for second animation
 
   function typeWriterHelper() {
     if (j < text.length) {
@@ -26,18 +29,7 @@ function typeWriter2() {
   typeWriterHelper();
 }
 
-// Check if the animation has already played
-window.onload = function () {
-  if (!localStorage.getItem("animationPlayed")) {
-    // Run the animations
-    typeWriter();
-    setTimeout(typeWriter2, 1000); // Start the second animation after 1 second
 
-    // Set a flag in localStorage to indicate the animation has been played
-    localStorage.setItem("animationPlayed", "true");
-  } else {
-    // Optionally, set static text if animations shouldn't replay
-    document.getElementById("name").innerHTML = name;
-    document.getElementById("study").innerHTML = "Engineering @ IMC Trading";
-  }
-};
+window.onload = function() { typeWriter(), typeWriter2()};
+
+
