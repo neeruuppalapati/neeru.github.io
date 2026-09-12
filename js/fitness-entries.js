c8ecf44413422086994a716165681f5a84d85739
Neerad Uppalapati <neeru@umich.edu>
2026-09-12 14:06:23 -0400

    Add September 10 and 12 fitness entries
    Adds the September 10 climb and run, plus the September 12 run, to the fitness log.

--- js/fitness-entries.js
+++ js/fitness-entries.js
@@ -8,8 +8,11 @@
 //   duration: how long it took
 
 export const entries = [
-  { date: "2026-09-04", activity: "5 mile run", duration: "40 minutes" },
-  { date: "2026-09-04", activity: "Climb",      duration: "2 hours" },
-  { date: "2026-09-05", activity: "4 mile run", duration: "45 minutes" },
-  { date: "2026-09-08", activity: "Climb",      duration: "2 hours" },
+  { date: '2026-09-04', activity: '5 mile run', duration: '40 minutes' },
+  { date: '2026-09-04', activity: 'Climb',      duration: '2 hours' },
+  { date: '2026-09-05', activity: '4 mile run', duration: '45 minutes' },
+  { date: '2026-09-08', activity: 'Climb',      duration: '2 hours' },
+  { date: '2026-09-10', activity: 'Climb',      duration: '2 hours' },
+  { date: '2026-09-10', activity: '4 mile run', duration: '36 minutes' },
+  { date: '2026-09-12', activity: '4 mile run', duration: '40 minutes' },
 ];
