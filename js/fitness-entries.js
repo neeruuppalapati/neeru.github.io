6c34b490b4a13791ebc6a603edc0a9607d553ed4
Neerad Uppalapati <neeru@umich.edu>
2026-09-22 23:27:12 -0400

    Add September 22 climb entry

--- js/fitness-entries.js
+++ js/fitness-entries.js
@@ -29,4 +29,5 @@ export const entries = [
   { date: '2026-09-20', activity: '4 mile run', duration: '36 minutes' },
   { date: '2026-09-21', activity: 'Basketball', duration: '2 hours' },
   { date: '2026-09-22', activity: 'Basketball', duration: '2 hours' },
+  { date: '2026-09-22', activity: 'Climb',      duration: '2 hours' },
 ];
